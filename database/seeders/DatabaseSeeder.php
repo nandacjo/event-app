14e88b44edd57bcbb82f37ec371e174925a9be6c
nandacjo <nandacoje22@gamil.com>
2023-07-14 09:01:15 -0400

    Membuat migrations | Laravel Event

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,6 +3,10 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+
+use App\Models\City;
+use App\Models\Country;
+use App\Models\Tag;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -14,9 +18,33 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Test User',
+            'email' => 'test@example.com',
+        ]);
+
+        $countries = [
+            'Indonesia',
+            'Malaysia',
+            'United Kingdom',
+            'Tahiland'
+        ];
+
+        for ($i=0; $i < count($countries); $i++) { 
+            Country::create([
+                'name' => $countries[$i],
+            ]);
+        }
+
+        City::create(['country_id' => 1, 'name' => 'Aceh']);
+        City::create(['country_id' => 1, 'name' => 'Medan']);
+        City::create(['country_id' => 1, 'name' => 'Palembang']);
+        City::create(['country_id' => 2, 'name' => 'Johor']);
+        City::create(['country_id' => 2, 'name' => 'Kuala Lumpur']);
+        City::create(['country_id' => 2, 'name' => 'Kuala']);
+
+        Tag::create(['name' => 'Larvel', 'slug' => 'laravel']);
+        Tag::create(['name' => 'Vue JS', 'slug' => 'vue-js']);
+        Tag::create(['name' => 'Livewire', 'slug' => 'livewire']);
     }
 }
